01b22b267ceff84cdf728ee3682d102b08c1d4a7
Nolovenodie <nolovenodie@outlook.com>
2021-05-24 15:39:31 +0800

    Update get_games_script.py

--- get_games_script.py
+++ get_games_script.py
@@ -5,22 +5,28 @@ from playwright.sync_api import sync_playwright
 
 def Run(Playwright, username, password):
     browser = None
-    print("初始化完成")
+    print(username+" 开始领取") 
+    
     try:
-        browser = Playwright.firefox.launch()
+        browser = playwright.firefox.launch()
         page = browser.new_page()
+        
         bot = EpicGamesBot(page)
-        print("正在登录。。。")
+        print("开始登录")
+            
         bot.log_in(None, username, password)
-        print("登录成功！")
+        print("登录成功")
+
         purchased_offer_urls = bot.purchase_free_promotional_offers()
+        print("领取完毕")
+        
         [print(url) for url in purchased_offer_urls]
-        print("{name} 领取完成".format(name=username))
+
         browser.close()
     except Exception:
         if browser:
             browser.close()
-            print("发生错误,领取失败")
+
         raise
 
 if __name__ == '__main__':
@@ -28,7 +34,8 @@ if __name__ == '__main__':
     Password = os.environ["EPIC_PASSWORD"]
     QmsgToken = os.environ["QMSG_TOKEN"]
     
-    CA = EpicGamesBot.list_free_promotional_offers()  # 免费游戏列表
-    print(CA)
+    Games = EpicGamesBot.list_free_promotional_offers()  # 游戏列表
+    print(Games)
+    
     with sync_playwright() as playwright:
         Run(playwright, Email, Password)
